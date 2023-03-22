289b4ca3116c40ae0bdfa34b09f67f9a257906c8
Younghun Lee <younghun5936@gmail.com>
2023-03-22 21:05:08 +0900

    week02-assignment

---
+++ week02-assignment/test_1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+
+	printf("Á¤¼ö 2°³ ");
+	scanf("%d %d", &a, &b);
+	printf("%d + %d = %d", a, b, a + b);
+	return 0;
+}
